d57224ec87d8e0c64d7f3cde5620687e62a34457
osnamagora <osnam.agora@gmail.com>
2021-07-29 15:42:03 +0200

    first commit

---
+++ src/main/java/com/osnam/initcicd/InitCicdApplication.java
@@ -0,0 +1,13 @@
+package com.osnam.initcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitCicdApplication.class, args);
+    }
+
+}
